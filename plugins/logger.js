5fde0ad394baf5048f5ff5e50a9a31a932ae6595
Ryan Houston <ryanhouston83@gmail.com>
2011-11-27 12:41:02 -0500

    Refactor logging plugin w/ functional object pattern

--- plugins/logger.js
+++ plugins/logger.js
@@ -1,20 +1,99 @@
-var request = require('request');
 var path = require('path');
 var fs = require('fs');
-
 var NerdieInterface = require('nerdie_interface.js');
-var logDir;
-var plugin;
-var enabledChannels = [];
 
-function Logger(parentNerdie) {
-  this.pluginInterface = new NerdieInterface(parentNerdie, this);
-  this.logEnabled = false;
-  plugin = this;
-  logDir = "logs";
+
+var Logger = function(parentNerdie) {
+
+  // Private variables
+  var that = {};
+  var pluginInterface = new NerdieInterface(parentNerdie, that);
+  var logEnabled = false;
+  var logDir = "logs";
+  var enabledChannels = [];
+
+  that.pluginInterface = pluginInterface;
+
+  var _isEnabledForSource = function (source) {
+    if (enabledChannels.indexOf(source) > -1) {
+      return true;
+    }
+
+    return false;
+  };
+
+  var _getTimestamp = function () {
+    var date = new Date();
+    var time = '[' + date.getHours() + ':' + date.getMinutes() + ']';
+
+    return time;
+  };
+
+  var _writeToLog = function (source, msg) {
+    if (!_isEnabledForSource(source)) {
+      return false;
+    }
+
+    var now = new Date();
+    var location = path.join(logDir, source);
+    var file = path.join( location, now.strftime( '%Y-%m-%d.log' ) );
+    path.exists( location, function( exists ) {
+      var log;
+      if (! exists ) {
+        console.log("Logger plugin creating path: " + location);
+        fs.mkdirSync( location, 0755 );
+      }
+      log = fs.createWriteStream ( file, { flags: 'a' });
+      log.write( msg + '\n');
+      log.end();
+    });
+
+    return true;
+  };
+
+  // Public function definitions
+  var logMessage = function(msg) {
+    _writeToLog(msg.source, msg);
+  };
+  that.logMessage = logMessage;
+
+  var logJoined = function (msg) {
+    var time = _getTimestamp();
+    var out = time + ' ' + msg.user + " has joined " + msg.source;
+    _writeToLog(msg.source, out);
+  };
+  that.logJoined = logJoined;
+
+  var logLeft = function (msg) {
+    var time = _getTimestamp();
+    var out = time + ' ' + msg.user + " has left " + msg.source;
+    _writeToLog(msg.source, out);
+  };
+  that.logLeft = logLeft;
+
+  var _registerPatterns = function() {
+    pluginInterface.registerPattern(
+      /.*/,
+      that.logMessage
+    );
+    pluginInterface.userJoin( function(msg) {
+      logJoined(msg);
+    });
+    pluginInterface.userLeave( function(msg) {
+      logLeft(msg);
+    });
+  }
+
+  var init = function() {
+    if (logEnabled) {
+      _registerPatterns();
+    }
+  }
+  that.init = init;
+
 
   if (parentNerdie.config.plugins.logger.enabled) {
-    this.logEnabled = true;
+    logEnabled = true;
   }
 
   if (parentNerdie.config.plugins.logger.dir) {
@@ -24,76 +103,10 @@ function Logger(parentNerdie) {
 
   if (parentNerdie.config.plugins.logger.channels) {
     enabledChannels = parentNerdie.config.plugins.logger.channels;
+    console.log("Logging channels: " + enabledChannels);
   }
 
-}
-
-Logger.prototype.init = function () {
-  if (this.logEnabled) {
-    this.pluginInterface.registerPattern(
-      /.*/,
-      this.logMessage
-    );
-    this.pluginInterface.userJoin( function(msg) {
-      plugin.logJoined(msg);
-    });
-    this.pluginInterface.userLeave( function(msg) {
-      plugin.logLeft(msg);
-    });
-  }
-}
-
-Logger.prototype.logMessage = function(msg) {
-  plugin.writeToLog(msg.source, msg);
-};
-
-Logger.prototype.writeToLog = function (source, msg) {
-  if (!this.isEnabledForSource(source)) {
-    return false;
-  }
-
-  var now = new Date();
-  var location = path.join(logDir, source);
-  var file = path.join( location, now.strftime( '%Y-%m-%d.log' ) );
-  path.exists( location, function( exists ) {
-    var log;
-    if (! exists ) {
-      console.log("Logger plugin creating path: " + location);
-      fs.mkdirSync( location, 0755 );
-    }
-    log = fs.createWriteStream ( file, { flags: 'a' });
-    log.write( msg + '\n');
-    log.end();
-  });
-
-  return true;
-};
-
-Logger.prototype.isEnabledForSource = function (source) {
-  if (enabledChannels.indexOf(source) > -1) {
-    return true;
-  }
-
-  return false;
-};
-
-Logger.prototype.logJoined = function (msg) {
-  var time = plugin.getTimestamp();
-  var out = time + ' ' + msg.user + " has joined " + msg.source;
-  plugin.writeToLog(msg.source, out);
-};
-
-Logger.prototype.logLeft = function (msg) {
-  var time = plugin.getTimestamp();
-  var out = time + ' ' + msg.user + " has left " + msg.source;
-  plugin.writeToLog(msg.source, out);
-};
-
-Logger.prototype.getTimestamp = function () {
-  var date = new Date();
-  var time = '[' + date.getHours() + ':' + date.getMinutes() + ']';
-
-  return time;
+  return that;
 };
 
 module.exports = Logger;
