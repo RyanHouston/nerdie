2233502743e11229dc0d2c245266f402894fa5f4
Ryan Houston <ryanhouston83@gmail.com>
2011-11-27 21:45:27 -0500

    Pad timestamps with 0s

--- plugins/logger.js
+++ plugins/logger.js
@@ -24,7 +24,18 @@ var Logger = function(parentNerdie) {
 
   var _getTimestamp = function () {
     var date = new Date();
-    var time = '[' + date.getHours() + ':' + date.getMinutes() + ']';
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+
+    if (hours < 10) {
+      hours = '0' + hours;
+    }
+
+    if (minutes < 10) {
+      minutes = '0' + minutes;
+    }
+
+    var time = '[' + hours + ':' + minutes + ']';
 
     return time;
   };
